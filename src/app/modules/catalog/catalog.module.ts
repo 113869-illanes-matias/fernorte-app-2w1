9b13973682f31d9c95e0e2bbf167c2f2ea243f19
Matias Illanes <illanesmatias04@gmail.com>
2023-10-19 12:48:51 -0300

    creacion de sidebar, componente y plantilla register

--- src/app/modules/catalog/catalog.module.ts
+++ src/app/modules/catalog/catalog.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterArticleComponent } from './components/register-article/register-article.component';
+
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, RegisterArticleComponent, ],
   providers: [],
   imports: [CommonModule],
   exports: [HomeComponent],
